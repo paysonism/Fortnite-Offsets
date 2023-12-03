dc9a55f579cb28f89272a2e1cb4a671f018720e5
Payson Holmes <paysonholmes@yahoo.com>
2023-12-03 09:39:21 -0700

    updated like 50 more

--- novoline.h
+++ novoline.h
@@ -1,6 +1,26 @@
 // latest offsets that are used for my novoline source.
 
-BoneMatrix = 0x149B864
-StaticFindObject = 0x3EDBF1C
+enum offsets : std::int32_t {
+	BoneMatrix = 0x149B864,
+	StaticFindObject = 0x3EDBF1C,
+	CurrentVehicle = 0x2840,
+	LastFireTime = 0xC98,
+	LastFireTimeVerified = 0xC9C,
+	CustomTimeDilation = 0x68,
+	GlobalAnimRateScale = 0xA68,
+	Mesh = 0x318,
+	bIsReloadingWeapon = 0x360,
+	VehicleAttributes = 0x10F0,
+	CachedSpeed = 0xD10,
+	TopSpeedCurrentMultiplier = 0x968,
+	PushForceCurrentMultiplier = 0x96C,
+	WaterEffectsVehicleMaxSpeedKmh = 0x7FC,
+	bAdsWhileNotOnGround = 0x5468,
+	bDisableEquipAnimation = 0x362,
+	bIgnoreTryToFireSlotCooldownRestriction = 0x12C9,
+	ZiplineState = 0x24E0,
+	TriggerType = 0xB4C,
+	WeaponData = 0x4A8,
+};
 
 // discord.gg/subz
