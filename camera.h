d028d779eb6f470a4853bd7bd0d218a8a6d207f7
Payson <paysonholmes@yahoo.com>
2024-02-07 08:35:10 -0700

    new camera

--- camera.h
+++ camera.h
@@ -1,28 +1,19 @@
-Decrypt Ansi
-
-char*v2; // rdi
-  int v4; // ebx
-  unsigned int v5; // er9
-  unsigned int v6; // er8
-  __int64 result; // rax
-  unsigned int v8; // er9
-  int v9; // ecx
+char *v2; // rdi
+  int v4; // ecx
+  unsigned int v5; // eax
+  __int64 v6; // rdx
 
   v2 = buffer;
-  v4 = length;
-  v5 = sub_14473C460(); //0x130D7A4
-  v6 = v5 ^ 0x9C677CC5;
-  result = 1041204193 * v5;
-  v8 = v5 % 0x21;
-  v9 = 0;
-  if ( v4 )
+  v4 = 9;
+  v5 = Length;
+  if ( v5 )
   {
+    v6 = v5;
     do
     {
-      result = v9 + v8;
-      ++v9;
-      v6 += result;
-      *v2++ ^= v6;
+      LOWORD(v5) = v4 + ~*v2;
+      v4 += 7947;
+      *v2++ = v5;
+      --v6;
     }
-    while ( v9 < v4 );
-  }
+    while ( v6 );
