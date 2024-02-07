5d6081aec53e6621a0ba374f872a09b72352170e
Payson <paysonholmes@yahoo.com>
2024-02-07 08:29:56 -0700

    new cam

--- camera.h
+++ camera.h
@@ -1,35 +1,28 @@
-struct payson1337
-{
-	Vector3 location;
-	Vector3 rotation;
-	float fov;
-};
+Decrypt Ansi
 
+char*v2; // rdi
+  int v4; // ebx
+  unsigned int v5; // er9
+  unsigned int v6; // er8
+  __int64 result; // rax
+  unsigned int v8; // er9
+  int v9; // ecx
 
-payson1337 get_camera()
-{
-	payson1337 camera;
-
-	auto location_pointer = read<uintptr_t>(Uworld + 0x110);
-	auto rotation_pointer = read<uintptr_t>(Uworld + 0x120);
-
-	struct FNRot
-	{
-		double a; //0x0000
-		char pad_0008[24]; //0x0008
-		double b; //0x0020
-		char pad_0028[424]; //0x0028
-		double c; //0x01D0
-	}fnRot;
-
-	fnRot.a = read<double>(rotation_pointer);
-	fnRot.b = read<double>(rotation_pointer + 0x20);
-	fnRot.c = read<double>(rotation_pointer + 0x1d0);
-
-	camera.location = read<Vector3>(location_pointer);
-	camera.rotation.x = asin(fnRot.c) * (180.0 / M_PI);
-	camera.rotation.y = ((atan2(fnRot.a * -1, fnRot.b) * (180.0 / M_PI)) * -1) * -1;
-	camera.fov = read<float>((uintptr_t)PlayerController + 0x394) * 90.f;
-
-	return camera;
-}
+  v2 = buffer;
+  v4 = length;
+  v5 = sub_14473C460(); //0x130D7A4
+  v6 = v5 ^ 0x9C677CC5;
+  result = 1041204193 * v5;
+  v8 = v5 % 0x21;
+  v9 = 0;
+  if ( v4 )
+  {
+    do
+    {
+      result = v9 + v8;
+      ++v9;
+      v6 += result;
+      *v2++ ^= v6;
+    }
+    while ( v9 < v4 );
+  }
