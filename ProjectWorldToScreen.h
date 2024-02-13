8f52f52e1c77787b4a42b007633296e0a55e2ac9
Payson <paysonholmes@yahoo.com>
2024-02-13 10:08:04 -0700

    w2s

--- ProjectWorldToScreen.h
+++ ProjectWorldToScreen.h
@@ -6,7 +6,7 @@ Vector3 ProjectWorldToScreen(Vector3 WorldLocation)
     Vector3 vAxisY = Vector3(tempMatrix.m[1][0], tempMatrix.m[1][1], tempMatrix.m[1][2]);
     Vector3 vAxisZ = Vector3(tempMatrix.m[2][0], tempMatrix.m[2][1], tempMatrix.m[2][2]);
     Vector3 vDelta = WorldLocation - ViewPoint.location;
-    Vector3 vTransformed = Vector3(vDelta.Dot(vAxisY), vDelta.Dot(vAxisZ), vDelta.Dot(vAxisX));
+    Vector3 vTransformed = Vector3(vDelta.Dot(vAxisY), vDelta.Dot(vAxisZ), vDelta.Dot(vAxisX))
     if (vTransformed.z < 1.f) // payson1337
         vTransformed.z = 1.f;
     return Vector3((Width / 2.0f) + vTransformed.x * (((Width / 2.0f) / tanf(ViewPoint.fov * (float)M_PI / 360.f))) / vTransformed.z, (Height / 2.0f) - vTransformed.y * (((Width / 2.0f) / tanf(ViewPoint.fov * (float)M_PI / 360.f))) / vTransformed.z, 0);
