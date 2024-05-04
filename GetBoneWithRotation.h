e683928289e5bc1ad38affff237266b104646a86
Payson <paysonholmes@yahoo.com>
2024-05-05 01:10:41 +0200

    new getbone

--- GetBoneWithRotation.h
+++ GetBoneWithRotation.h
@@ -1,14 +1,17 @@
-uintptr_t GetBoneArrayAddress(uintptr_t mesh) {
-    uintptr_t bone_array = d::read<uintptr_t>(mesh + 0x608);
-    if (bone_array == NULL) // payson1337
-        bone_array = d::read<uintptr_t>( mesh + 0x608 + 0x10);
-    return bone_array;
+ftransform GetBoneIndex(uint64_t mesh, int index)
+{
+	uint64_t bonearray = read<uint64_t>(mesh + 0x4a8);
+	if (!bonearray) bonearray = read<uint64_t>(mesh + 0x4a8 + 0x10);
+	return read<ftransform>(bonearray + (index * 0x30));
 }
 
-Vector3 GetBoneWithRotation(uintptr_t mesh, int bone_id) {
-    uintptr_t bone_array = GetBoneArrayAddress((u)this);
-    FTransform bone = d::read <FTransform>(bone_array + (bone_id * 0x60));
-    FTransform component_to_world = d::read<FTransform>((u)this + 0x230);
-    D3DMATRIX matrix = MatrixMultiplication(bone.ToMatrixWithScale(), component_to_world.ToMatrixWithScale());
-    return Vector3(matrix._41, matrix._42, matrix._43);
+Vector3 GetBoneWithRotation(DWORD_PTR mesh, int id)
+{
+	ftransform bone = GetBoneIndex(mesh, id);
+	ftransform ComponentToWorld = read<ftransform>(mesh + 0x1c0);
+
+	D3DMATRIX Matrix;
+	Matrix = MatrixMultiplication(bone.ToMatrixWithScale(), ComponentToWorld.ToMatrixWithScale());
+
+	return Vector3(Matrix._41, Matrix._42, Matrix._43);
 }
