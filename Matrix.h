e99b179f2ceed3215b1b8f4de50c4f941330f751
Payson <paysonholmes@yahoo.com>
2024-05-05 01:07:58 +0200

    matrix

---
+++ Matrix.h
@@ -0,0 +1,71 @@
+struct ftransform
+{
+	fplane  rot;
+	FVector translation;
+	char    pad[8];
+	FVector scale;
+
+	D3DMATRIX ToMatrixWithScale()
+	{
+		D3DMATRIX m;
+		m._41 = translation.x;
+		m._42 = translation.y;
+		m._43 = translation.z;
+
+		double x2 = rot.x + rot.x;
+		double y2 = rot.y + rot.y;
+		double z2 = rot.z + rot.z;
+
+		double xx2 = rot.x * x2;
+		double yy2 = rot.y * y2;
+		double zz2 = rot.z * z2;
+		m._11 = (1.0f - (yy2 + zz2)) * scale.x;
+		m._22 = (1.0f - (xx2 + zz2)) * scale.y;
+		m._33 = (1.0f - (xx2 + yy2)) * scale.z;
+
+		double yz2 = rot.y * z2;
+		double wx2 = rot.W * x2;
+		m._32 = (yz2 - wx2) * scale.z;
+		m._23 = (yz2 + wx2) * scale.y;
+
+		double xy2 = rot.x * y2;
+		double wz2 = rot.W * z2;
+		m._21 = (xy2 - wz2) * scale.y;
+		m._12 = (xy2 + wz2) * scale.x;
+
+		double xz2 = rot.x * z2;
+		double wy2 = rot.W * y2;
+		m._31 = (xz2 + wy2) * scale.z;
+		m._13 = (xz2 - wy2) * scale.x;
+
+		m._14 = 0.0f;
+		m._24 = 0.0f;
+		m._34 = 0.0f;
+		m._44 = 1.0f;
+
+		return m;
+	}
+};
+
+D3DMATRIX MatrixMultiplication(D3DMATRIX pM1, D3DMATRIX pM2)
+{
+	D3DMATRIX pOut;
+	pOut._11 = pM1._11 * pM2._11 + pM1._12 * pM2._21 + pM1._13 * pM2._31 + pM1._14 * pM2._41;
+	pOut._12 = pM1._11 * pM2._12 + pM1._12 * pM2._22 + pM1._13 * pM2._32 + pM1._14 * pM2._42;
+	pOut._13 = pM1._11 * pM2._13 + pM1._12 * pM2._23 + pM1._13 * pM2._33 + pM1._14 * pM2._43;
+	pOut._14 = pM1._11 * pM2._14 + pM1._12 * pM2._24 + pM1._13 * pM2._34 + pM1._14 * pM2._44;
+	pOut._21 = pM1._21 * pM2._11 + pM1._22 * pM2._21 + pM1._23 * pM2._31 + pM1._24 * pM2._41;
+	pOut._22 = pM1._21 * pM2._12 + pM1._22 * pM2._22 + pM1._23 * pM2._32 + pM1._24 * pM2._42;
+	pOut._23 = pM1._21 * pM2._13 + pM1._22 * pM2._23 + pM1._23 * pM2._33 + pM1._24 * pM2._43;
+	pOut._24 = pM1._21 * pM2._14 + pM1._22 * pM2._24 + pM1._23 * pM2._34 + pM1._24 * pM2._44;
+	pOut._31 = pM1._31 * pM2._11 + pM1._32 * pM2._21 + pM1._33 * pM2._31 + pM1._34 * pM2._41;
+	pOut._32 = pM1._31 * pM2._12 + pM1._32 * pM2._22 + pM1._33 * pM2._32 + pM1._34 * pM2._42;
+	pOut._33 = pM1._31 * pM2._13 + pM1._32 * pM2._23 + pM1._33 * pM2._33 + pM1._34 * pM2._43;
+	pOut._34 = pM1._31 * pM2._14 + pM1._32 * pM2._24 + pM1._33 * pM2._34 + pM1._34 * pM2._44;
+	pOut._41 = pM1._41 * pM2._11 + pM1._42 * pM2._21 + pM1._43 * pM2._31 + pM1._44 * pM2._41;
+	pOut._42 = pM1._41 * pM2._12 + pM1._42 * pM2._22 + pM1._43 * pM2._32 + pM1._44 * pM2._42;
+	pOut._43 = pM1._41 * pM2._13 + pM1._42 * pM2._23 + pM1._43 * pM2._33 + pM1._44 * pM2._43;
+	pOut._44 = pM1._41 * pM2._14 + pM1._42 * pM2._24 + pM1._43 * pM2._34 + pM1._44 * pM2._44;
+
+	return pOut;
+}
