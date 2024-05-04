75c6adfa05aabec5ffcd4fc237f90e9b04b81bc7
Payson <paysonholmes@yahoo.com>
2024-05-05 01:03:41 +0200

    w2s

--- ProjectWorldToScreen.h
+++ ProjectWorldToScreen.h
@@ -1,13 +1,47 @@
 Vector3 ProjectWorldToScreen(Vector3 WorldLocation)
 {
-    CamewaDescwipsion ViewPoint = get_camera();
-    D3DMATRIX tempMatrix = Matrix(ViewPoint.rotation);
-    Vector3 vAxisX = Vector3(tempMatrix.m[0][0], tempMatrix.m[0][1], tempMatrix.m[0][2]);
-    Vector3 vAxisY = Vector3(tempMatrix.m[1][0], tempMatrix.m[1][1], tempMatrix.m[1][2]);
-    Vector3 vAxisZ = Vector3(tempMatrix.m[2][0], tempMatrix.m[2][1], tempMatrix.m[2][2]);
-    Vector3 vDelta = WorldLocation - ViewPoint.location;
-    Vector3 vTransformed = Vector3(vDelta.Dot(vAxisY), vDelta.Dot(vAxisZ), vDelta.Dot(vAxisX))
-    if (vTransformed.z < 1.f) // payson1337
-        vTransformed.z = 1.f;
-    return Vector3((Width / 2.0f) + vTransformed.x * (((Width / 2.0f) / tanf(ViewPoint.fov * (float)M_PI / 360.f))) / vTransformed.z, (Height / 2.0f) - vTransformed.y * (((Width / 2.0f) / tanf(ViewPoint.fov * (float)M_PI / 360.f))) / vTransformed.z, 0);
+	Vector3 Screenlocation = Vector3(0, 0, 0);
+	Vector3 Camera;
+
+	auto chain69 = read<uintptr_t>(offset::local_player + 0xA8);
+	uint64_t chain699 = read<uintptr_t>(chain69 + 8);
+
+	Camera.x = read<float>(chain699 + 0x7F8);
+
+	Camera.y = read<float>(offset::root_component + 0x12C);
+
+	float test = asin(Camera.x);
+	float degrees = test * (180.0 / M_PI);
+	Camera.x = degrees;
+
+	if (Camera.y < 0)
+		Camera.y = 360 + Camera.y;
+
+	D3DMATRIX tempMatrix = Matrix2(Camera);
+	Vector3 vAxisX, vAxisY, vAxisZ;
+
+	vAxisX = Vector3(tempMatrix.m[0][0], tempMatrix.m[0][1], tempMatrix.m[0][2]);
+	vAxisY = Vector3(tempMatrix.m[1][0], tempMatrix.m[1][1], tempMatrix.m[1][2]);
+	vAxisZ = Vector3(tempMatrix.m[2][0], tempMatrix.m[2][1], tempMatrix.m[2][2]);
+
+	uint64_t chain = read<uint64_t>(offset::local_player + 0x70);
+	uint64_t chain1 = read<uint64_t>(chain + 0x98);
+	uint64_t chain2 = read<uint64_t>(chain1 + 0x140);
+    
+	Vector3 vDelta = WorldLocation - read<Vector3>(chain2 + 0x10);
+	Vector3 vTransformed = Vector3(vDelta.Dot(vAxisY), vDelta.Dot(vAxisZ), vDelta.Dot(vAxisX));
+
+	if (vTransformed.z < 1.f)
+		vTransformed.z = 1.f;
+
+	zoom = read<float>(chain699 + 0x590);
+
+	float FovAngle = 80.0f / (zoom / 1.19f);
+	float ScreenCenterX = Width / 2.0f;
+	float ScreenCenterY = Height / 2.0f;
+
+	Screenlocation.x = ScreenCenterX + vTransformed.x * (ScreenCenterX / tanf(FovAngle * (float)M_PI / 360.f)) / vTransformed.z;
+	Screenlocation.y = ScreenCenterY - vTransformed.y * (ScreenCenterX / tanf(FovAngle * (float)M_PI / 360.f)) / vTransformed.z;
+
+	return Screenlocation;
 }
