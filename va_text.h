4e37311a82b74c401b35721f22d0761e57c096db
eramp <ogfnzerogravity@gmail.com>
2024-09-04 10:46:28 -0700

    Update va_text.h

--- va_text.h
+++ va_text.h
@@ -1,9 +1,9 @@
 		__int64 va_text = 0;
 	for (auto i = 0; i < FLT_MAX; i++) {
 		va_text = virtualaddy + i * 0x1000;
-		auto uworld = read<uintptr_t>(va_text + GWORLD);
-		auto level = read<uintptr_t>(uworld + PersistentLevel);
-		if (uworld && level && read<uintptr_t>(level + OwningWorld) == uworld) {
+		auto uworld = read<uintptr_t>(va_text + offsets::uworld);
+		auto level = read<uintptr_t>(uworld + offsets::PersistentLevel);
+		if (uworld && level && read<uintptr_t>(level + offsets::OwningWorld) == uworld) {
 			break;
 		}
 	}
