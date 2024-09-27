1808f620017d74955f35eeb4bc7ee099e832d796
eramp <ogfnzerogravity@gmail.com>
2024-09-27 14:00:42 -0300

    fixed cr3 by pass.h

--- cr3 by pass.h
+++ cr3 by pass.h
@@ -1,20 +1,15 @@
-void SigmaMigma() {
-    auto previous_cr3 = mem::fetch_cr3();
-    int counter = 0; 
-    while (true)
-    {
-        if (!cache::gworld) {
-            mem::fetch_cr3();
-            std::this_thread::sleep_for(std::chrono::microseconds(100));
-        }
-    }
-}
 
+void cr3_loop()
+{
+	for (;;)
+	{
+		mem::CR3();
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+}
 
 
 and on ur int main()
 {
-    std::thread funcThread(SigmaMigma);
-    HANDLE threadHandle = funcThread.native_handle();
-    threadHandle.Detach();
+std::thread([&]() { for (;;) { cr3_loop(); } }).detach();
 }
