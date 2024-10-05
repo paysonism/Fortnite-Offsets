cd61b1a41f3d6ae60341cd43ee3d42df42ee6ee7
eramp <ogfnzerogravity@gmail.com>
2024-10-05 20:12:29 -0300

    Update va_text.h

--- va_text.h
+++ va_text.h
@@ -1,7 +1,7 @@
 		__int64 va_text = 0;
     for (auto i = 0; i < FLT_MAX; i++) {
         va_text = virtualaddy + i * 0x1000;
-        auto uworld = read<uintptr_t>(va_text + 0x1215C178);
+        auto uworld = read<uintptr_t>(va_text + 0x12300468);
         auto level = read<uintptr_t>(uworld + 0x30);
         if (uworld && level && read<uintptr_t>(level + 0xc0) == uworld) {
             break;
