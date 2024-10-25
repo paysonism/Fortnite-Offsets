778e490714af891b4f218aab98f6444b6353c2de
eramp <ogfnzerogravity@gmail.com>
2024-10-25 05:51:05 -0700

    UPDATE 31.41!

--- offsets.h
+++ offsets.h
@@ -1,11 +1,11 @@
 namespace Addresses
 {
-	uintptr_t GWorld = 0x12200EE8; // (UWorld) // fresh dump
+	uintptr_t GWorld = 0x1221B738; // (UWorld) // fresh dump
         uintptr_t PersistentLevel = 0x30
         uintptr_t OwningWorld = 0xc0
   uintptr_t HabaneroComponent = 0x9E8;// fresh dump
-	uintptr_t GObjects = 0x122A6A80;  // fresh dump
-	uintptr_t GNames = 0x124AE500;// fresh dump
+	uintptr_t GObjects = 0x121F3380;  // fresh dump
+	uintptr_t GNames = 0x123FAFC0;// fresh dump
 	uintptr_t FName = 0x124DE7C0 + 8;
 	uintptr_t StaticFindObject = 0x1DE185C;
    	uintptr_t GetBoneMatrix = 0x1E4E8E6;
