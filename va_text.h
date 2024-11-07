3cecf221a70310df7723f298fbeb49ecd261d973
eramp <ogfnzerogravity@gmail.com>
2024-11-07 11:17:26 -0800

    Update va_text.h

--- va_text.h
+++ va_text.h
@@ -1,8 +1,10 @@
-		__int64 va_text = 0;
+// U DONT NEED ANYMORE VA_TEXT JUST USE VIRTUALADDY + UWORLD OR UR BASEID MET + UWORLD BUT IF U STILL USE IT HERE IS THE NEW UPDATED 1
+
+__int64 va_text = 0;
     for (auto i = 0; i < FLT_MAX; i++) {
         va_text = virtualaddy + i * 0x1000;
         auto uworld = read<uintptr_t>(va_text + 0x12300E58);
-        auto level = read<uintptr_t>(uworld + 0x30);
+        auto level = read<uintptr_t>(uworld + 0x38);
         if (uworld && level && read<uintptr_t>(level + 0xc0) == uworld) {
             break;
         }
