54ddbbcf5bd8265d15db3c2811a4b4c858660e0c
eramp <ogfnzerogravity@gmail.com>
2025-07-15 10:39:55 +0000

    Update cr3 by pass.h

--- cr3 by pass.h
+++ cr3 by pass.h
@@ -4,7 +4,7 @@ void cr3_loop()
 	for (;;)
 	{
 		mem::fetch_cr3();
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::this_thread::sleep_for(std::chrono::milliseconds(300));
 	}
 }
 
