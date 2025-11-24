81a55968e14e1ccb266cb96c39e1232a7852a217
Payson <paysonholmes@yahoo.com>
2025-11-24 14:00:09 -0700

    Update VisCheck.h

--- VisCheck.h
+++ VisCheck.h
@@ -1,7 +1,7 @@
-// Current Patch: v38.10
+// Current Patch: v38.11
 
 bool IsEnemyVisible(uintptr_t Mesh) {
 	auto Seconds = kernel->read_t<double>(cache::Gworld + offsets::CameraRotation + 0x10);
-	auto LastRenderTime = kernel->read_t<float>(Mesh + 0x32C);
+	auto LastRenderTime = kernel->read_t<float>(Mesh + 0x32C); // note for later: add this to offsets struct for easier updating
 	return Seconds - LastRenderTime <= 0.06f;
 }
