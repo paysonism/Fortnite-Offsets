a5ff88c935c4476885b63b12901ca262512c38e7
Payson <paysonholmes@yahoo.com>
2025-12-12 22:42:06 -0700

    v39.10

--- GObjects.h
+++ GObjects.h
@@ -1,37 +1,40 @@
 // Current Patch: v39.10
 
-uint32_t GetObjectsCount()
+uint32_t GetObjectCount()
 {
-    const uintptr_t GObjectsCountEncryptedRVA = offsets::GObjectsCount;
-    const uint32_t XorKey = 0xBC0BA8E;
-
-    const uint32_t encryptedCount = read<uint32_t>(BaseAddress + GObjectsCountEncryptedRVA);
-
-    return (encryptedCount ^ XorKey);
+    const uintptr_t GObjectsCountEncryptedRVA = offsets::GObjects;
+    const uintptr_t xorKey = offsets::GObjectsXorKey;
+ 
+    const uint32_t encryptedCount = Read<uint32_t>(Memory.ModuleBase + GObjectsCountEncryptedRVA);
+ 
+    return encryptedCount ^ xorKey;
 }
  
-uint64_t GetObjectByIndex(uint32_t index)
+uint64_t GetUObjectFromID(uint32_t index)
 {
-    if (!BaseAddress) return 0;
-
+    if (!Memory.ModuleBase) return 0;
+ 
     const uintptr_t GObjectsEncryptedRVA = offsets::GObjects;
-    const uint64_t GObjectsArrayKey = 0xFFFFFFFF91478091ULL;
-    const uint64_t encrypted_gobjects = read<uint64_t>(BaseAddress + GObjectsEncryptedRVA);
-    const uintptr_t gObjectsChunkTable = RotateLeft64(encrypted_gobjects, 47) - GObjectsArrayKey;
-
+    const uint64_t GObjectsArrayKey = offsets::GObjectsArrayXorKey;
+ 
+    const uint64_t encrypted_gobjects = Read<uint64_t>(Memory.ModuleBase + GObjectsEncryptedRVA);
+ 
+    const uintptr_t gObjectsChunkTable = encrypted_gobjects ^ GObjectsArrayKey;
+ 
     const int32_t chunk_index = index >> 16;
-    const uintptr_t chunk_address = read<uintptr_t>(gObjectsChunkTable + (chunk_index * 8LL));
-    if (!chunk_address) return 0;
-
+    const uintptr_t chunk_address = Read<uintptr_t>(gObjectsChunkTable + (chunk_index * 8LL));
+ 
     const int32_t index_in_chunk = index & 0xFFFF;
     const uintptr_t item_address = chunk_address + (index_in_chunk * 24);
-
-    const uint64_t item_raw_value = read<uint64_t>(item_address);
-    const uint32_t encrypted_lower_part = read<uint32_t>(item_address + 8);
-
+ 
+    const uint64_t item_raw_value = Read<uint64_t>(item_address);
+    const uint32_t encrypted_lower_part = Read<uint32_t>(item_address + 8);
+ 
     const uint32_t lower_decrypted = (encrypted_lower_part ^ 0x8AB29D8ELL);
+ 
     const uint64_t upper_part = item_raw_value & 0x3FFF00000000LL;
+ 
     const uint64_t reconstructed_value = (uint64_t)lower_decrypted | upper_part;
-
+ 
     return reconstructed_value * 8;
 }
--- offsets.h
+++ offsets.h
@@ -11,81 +11,82 @@ namespace offsets {
     uintptr_t UWorldXorKey = 0xFFFFFFFFDC445031ULL;
     uintptr_t GNames = 0x175746C0; // updated: 0x17168D00 -> 0x175746C0
     uintptr_t GObjects = 0x176515C8; // updated: 0x172467F0 -> 0x176515C8
+    uintptr_t GObjectsXorKey = 0xBC0BA8E;
+    uintptr_t GObjectsArrayXorKey = 0xFFFFFFFF91478091ULL;
     uintptr_t GObjectsCount = 0x176515C0; // updated: 0x17246804 -> 0x176515C0
     uintptr_t StaticFindObject = 0x64618E; // updated: 0x83F8E -> 0x64628E
 
     uintptr_t ProcessEvent = 0x83A74;
     uintptr_t ProcessEventIndex = 0x78;
 
-    uintptr_t LocalPlayers = 0x38;
+    uintptr_t LocalPlayers = 0x38; // SDK
     uintptr_t PlayerController = 0x30;
     uintptr_t PlayerCameraManager = 0x368; // updated: 0x360 -> 0x368
-    uintptr_t AcknowledgedPawn = 0x358;
-    uintptr_t PlayerState = 0x2D0;
+    uintptr_t AcknowledgedPawn = 0x358; // SDK
+    uintptr_t PlayerState = 0x2B8; // updated: 0x2D0 -> 0x2B8 : SDK
     uintptr_t TeamIndex = 0x11A9;
     uintptr_t bIsDying = 0x728;
     uintptr_t bIsDBNO = 0x841;
     uintptr_t bIsABot = 0x2BA;
 
-    uintptr_t Platform = 0x400;
-    uintptr_t TargetedFortPawn = 0x1830; // updated: 0x1820 -> 0x1830
-    uintptr_t KillScore = 0x11C0;
+    uintptr_t Platform = 0x440; // updated: 0x400 -> 0x440 : SDK
+    uintptr_t TargetedFortPawn = 0x1840; // updated: 0x1830 -> 0x1840
+    uintptr_t KillScore = 0x11C0; // SDK
     uintptr_t PlayerName = 0xA00;
-    uintptr_t RankedProgress = 0xD8;
-    uintptr_t PlayerAimOffset = 0x2BE8;
+    uintptr_t RankedProgress = 0xD8; // SDK
+    uintptr_t PlayerAimOffset = 0x2BF8; // updated: 0x2BE8 -> 0x2BF8
 
     uintptr_t OwningGameInstance = 0x240; // updated: 0x248 -> 0x240
-    uintptr_t GameState = 0x1D0;
+    uintptr_t GameState = 0x1C8; // updated: 0x1D0 -> 0x1C8 : SDK
     uintptr_t PlayerArray = 0x2C8;
-    uintptr_t WorldSettings = 0x2B8;
-    uintptr_t WorldGravityZ = 0x330;
-    uintptr_t WorldToMeters = 0x320;
+    uintptr_t WorldSettings = 0x2B8; // SDK
+    uintptr_t WorldGravityZ = 0x330; // SDK
+    uintptr_t WorldToMeters = 0x320; // SDK
     uintptr_t WorldTick = 0x198;
 
-    uintptr_t RootComponent = 0x1B0;
-    uintptr_t PawnPrivate = 0x328;
+    uintptr_t RootComponent = 0x1B0; // SDK
+    uintptr_t PawnPrivate = 0x328; // SDK
     uintptr_t AActor = 0x38;
 
-    uintptr_t PersistentLevel = 0x40; // updated: 0x38 -> 0x40
-    uintptr_t Levels = 0x1E8;
+    uintptr_t PersistentLevel = 0x38;
+    uintptr_t Levels = 0x1E0; // updated: 0x1E8 -> 0x1E0
     uintptr_t SuperField = 0x40;
-    uintptr_t StaticFindObject = 0x83F8E;
 
-    uintptr_t Mesh = 0x330;
+    uintptr_t Mesh = 0x330; // SDK
     uintptr_t BoneArray = 0x5F0;
     uintptr_t BoneCache = 0x5F8;
     uintptr_t BonesTide = 0x60;
 
-    uintptr_t ComponentToWorld = 0x1A0; // updated: 0x1E0 -> 0x1A0
-    uintptr_t RelativeLocation = 0x140;
-    uintptr_t RelativeScale3D = 0x170;
-    uintptr_t ComponentVelocity = 0x188;
-    uintptr_t AdditionalAimOffset = 0x2BB8;
+    uintptr_t ComponentToWorld = 0x1A0; // updated: 0x1E0 -> 0x1A0 : SDK
+    uintptr_t RelativeLocation = 0x140; // SDK
+    uintptr_t RelativeScale3D = 0x170; // SDK
+    uintptr_t ComponentVelocity = 0x188; // SDK
+    uintptr_t AdditionalAimOffset = 0x2BC8; // updated: 0x2BB8 -> 0x2BC8 : SDK
     uintptr_t LastRenderTime = 0x32C;
-    uintptr_t LocationUnderReticle = 0x2A68;
+    uintptr_t LocationUnderReticle = 0x2A78; // updated: 0x2A68 -> 0x2A78 : SDK
 
     uintptr_t CameraLocation = 0x170; // updated: 0x178 -> 0x170
     uintptr_t CameraRotation = 0x180; // updated: 0x188 -> 0x180
     uintptr_t CameraFOV = 0x3B4;
 
-    uintptr_t CurrentVehicle = 0x2B78;
+    uintptr_t CurrentVehicle = 0x2C48; // updated: 0x2B78 -> 0x2C48 : SDK
 
-    uintptr_t CurrentWeapon = 0x990;
-    uintptr_t WeaponData = 0x5C0; // updated: 0x5D8 -> 0x5C0
-    uintptr_t AmmoCount = 0x1464; // updated: 0x144C -> 0x1464
-    uintptr_t ReloadAnimation = 0x1940; // updated: 0x19E8 -> 0x1940
-    uintptr_t ItemName = 0x40;
+    uintptr_t CurrentWeapon = 0x990; // SDK
+    uintptr_t WeaponData = 0x5C0; // updated: 0x5D8 -> 0x5C0 : SDK
+    uintptr_t AmmoCount = 0x1464; // updated: 0x144C -> 0x1464 : SDK
+    uintptr_t ReloadAnimation = 0x1928; // updated: 0x1940 -> 0x1928 : SDK
+    uintptr_t ItemName = 0x40; // SDK
 
     uintptr_t ProjectileSpeed = 0x2750; // updated: 0x212C -> 0x2750
     uintptr_t ProjectileGravity = 0x2754; // updated: 0x2130 -> 0x2754
-    uintptr_t MaxTargetingAimAdjustPerSecond = 0x2338; // needs testing
-    uintptr_t ServerWorldTimeSecondsDelta = 0x2E8; // needs testing
+    uintptr_t MaxTargetingAimAdjustPerSecond = 0x2338; // updated: 0x2338 -> 0x23A0 : SDK
+    uintptr_t ServerWorldTimeSecondsDelta = 0x2E8; // SDK
 
-    uintptr_t PrimaryPickupItemEntry = 0x3A8; // needs testing
+    uintptr_t PrimaryPickupItemEntry = 0x3A8; // SDK
     uintptr_t ItemTier = 0xA2; // needs testing
     uintptr_t ItemRarity = 0xAA; // needs testing
-    uintptr_t bAlreadySearched = 0xD52;
-    uintptr_t HabaneroComponent = 0x940; // updated: 0x938 -> 0x940
+    uintptr_t bAlreadySearched = 0xD52; // SDK
+    uintptr_t HabaneroComponent = 0x940; // updated: 0x938 -> 0x940 : SDK
 };
 
 // Offsets from https://github.com/paysonism/Fortnite-Offsets
