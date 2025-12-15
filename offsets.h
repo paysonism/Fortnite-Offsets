285f38c4eac20d3baac9b24f8eac2b71d8c2c3f4
Payson <paysonholmes@yahoo.com>
2025-12-15 12:05:05 -0700

    39.10

--- offsets.h
+++ offsets.h
@@ -91,8 +91,8 @@ namespace offsets {
     uintptr_t ReloadAnimation = 0x1928;
     uintptr_t LWProjectile_ActivateRemovedTimestamp = 0x29F8;
 
-    uintptr_t ProjectileSpeed = 0x2750;
-    uintptr_t ProjectileGravity = 0x2754;
+    uintptr_t ProjectileSpeed = 0x26E4;
+    uintptr_t ProjectileGravity = 0x26E8;
     uintptr_t MaxTargetingAimAdjustPerSecond = 0x2338;
     uintptr_t ServerWorldTimeSecondsDelta = 0x2E8;
 
